f017eaf249a27746a65d374def084c42723aafd2
cyb3r14 <177793202+cyb3r14@users.noreply.github.com>
2024-11-07 23:04:39 -0600

    first commit

---
+++ .dwm/autostart.sh
@@ -0,0 +1,7 @@
+feh --bg-fill /home/anon/.wallpapers/wall1.jpg
+slstatus &
+pipewire &
+/usr/bin/pipewire-pulse &
+/usr/bin/wireplumber &
+nm-applet &
+dwm
---
+++ README.md
@@ -0,0 +1,5 @@
+# Dotfiles
+In this repository i store my dwm config, you can try it if you want idk
+
+# Screenshot
+![showcase](assets/showcase.png)
